f1c96680f762b6c2e896912dc6074489cc3f0be0
evopen <520dhh@gmail.com>
2021-01-27 12:20:20 +0800

    fluid 2d implemented in rust-gpu

--- shaders/d2-shader/src/lib.rs
+++ shaders/d2-shader/src/lib.rs
@@ -8,26 +8,21 @@
 #[cfg(not(target_arch = "spirv"))]
 #[macro_use]
 pub extern crate spirv_std_macros;
-use spirv_std::glam::{vec4, Vec4};
+
+use spirv_std::glam::{vec2, vec4, Vec2, Vec2Swizzles, Vec4};
 use spirv_std::storage_class::{Input, Output};
 
 #[allow(unused_attributes)]
 #[spirv(fragment)]
 pub fn main_fs(mut output: Output<Vec4>) {
-    output.store(vec4(1.0, 0.0, 0.0, 1.0))
+    output.store(vec4(0.2, 0.6, 1.0, 1.0))
 }
 
 #[allow(unused_attributes)]
 #[spirv(vertex)]
-pub fn main_vs(
-    #[spirv(vertex_index)] vert_id: Input<i32>,
-    #[spirv(position)] mut out_pos: Output<Vec4>,
-) {
-    let vert_id = vert_id.load();
-    out_pos.store(vec4(
-        (vert_id - 1) as f32,
-        ((vert_id & 1) * 2 - 1) as f32,
-        0.0,
-        1.0,
-    ));
+pub fn main_vs(a_pos: Input<Vec2>, #[spirv(position)] mut out_pos: Output<Vec4>) {
+    let mut a_pos = a_pos.load();
+    a_pos /= vec2(800.0, 600.0) * 1.5;
+    a_pos = (a_pos * 2.0) - vec2(1.0, 1.0);
+    out_pos.store(vec4(a_pos.x, a_pos.y, 0.0, 1.0));
 }
